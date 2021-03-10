51e9284f0956f260c9b066c30bea442bdedbe06f
Duduxs <duduxss3@gmail.com>
2021-03-10 08:40:36 -0300

    Oracle second module done

--- desafios/dio-bootcamp-java/src/Hi.java
+++ desafios/dio-bootcamp-java/src/Hi.java
@@ -1,7 +1,31 @@
 
 public class Hi {
 	String ObjectSystemValue = "";
+
 	public static void main(String[] ee) {
-		System.out.println(ee[0] + ee[1]);
-}
+//		System.out.println(ee[0] + ee[1]);
+		byte a = 125, b = 5;
+		b+= 1;
+		System.out.println(b);
+		
+		int c = 11, d = 3;
+		double e = Math.round((float)c/d);
+		System.out.println(e);
+	
+		int i = 0;
+		algo(i);
+		System.out.println(i);
+		
+		String ax = new String("Hello");
+		String bx = ax.intern();
+		bx = (bx+1);
+		
+		System.out.println(ax.substring(0,5));
+		
+		ax.intern();
+	}
+	
+	public static void algo(int i) {
+		++i;
+	}
 }
