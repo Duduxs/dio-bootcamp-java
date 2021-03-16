ff5460606fe3bc674e3bd7a35dd0cfcacc30a558
Duduxs <duduxss3@gmail.com>
2021-03-15 21:30:11 -0300

    Classe PassByValue adicionada

---
+++ desafios/dio-bootcamp-java/src/oracle/labs/practice2/PassByValue.java
@@ -0,0 +1,27 @@
+package oracle.labs.practice2;
+
+import duke.choice.Customer;
+
+public class PassByValue {
+
+	public static void changeIt(Customer object, int x, String o) {
+		object = new Customer("Blue", 4);
+		x = 500;
+		o = "Ui";
+		o.substring(1);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Duke Choice Shop!");
+		Customer c1 = new Customer("Pinky", 3);
+		String o = "Oi, blz?!";
+		String a = new String("Oi, blz?!");
+		int x = 1;
+		
+		changeIt(c1, x, o);
+		System.out.println(c1.getName());
+		System.out.println(x);
+		System.out.println(o == a);
+	}
+}
